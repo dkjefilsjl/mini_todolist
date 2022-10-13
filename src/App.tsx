1ca3f47a81bfa4567218b4eb495e6a04b331f84d
dkjefilsjl <6792kjs@naver.com>
2022-10-14 01:32:37 +0900

    npm styled component version 충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Header } from "./components/header/header";
 import TodoList from "./containers/todo-list/todo-list";
 
 function App() {
   return (
     <Router basename={"/"}>
+      <Header />
       <Routes>
         <Route path="" element={<TodoList />} />
       </Routes>
---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const MainText = styled.div`
+  font-size: 5rem;
+`;
+
+export const Header = () => {
+  return (
+    <>
+      <MainText>To do List</MainText>
+    </>
+  );
+};
